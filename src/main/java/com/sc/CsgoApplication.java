db7b02221eabaea71864f02d922101da494da75d
Aperson-plus <3296931330@qq.com>
2021-07-14 20:18:25 +0800

    didi

---
+++ src/main/java/com/sc/CsgoApplication.java
@@ -0,0 +1,13 @@
+package com.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsgoApplication.class, args);
+	}
+
+}
