7450e9356ed542e1cf7205883a8c3acd0cd2d35e
woshinidiedeyeye <582965041@qq.com>
2021-07-15 18:34:25 +0800

    3214

--- src/main/java/com/sc/controller/ItemController.java
+++ src/main/java/com/sc/controller/ItemController.java
@@ -1,16 +1,12 @@
 package com.sc.controller;
-
-import com.sc.service.ItemService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping("/item")
 public class ItemController {
-    @Autowired
-    ItemService itemService;
+
 
 
 }
---
+++ src/test/java/com/sc/dao/BuyerDaoTest.java
@@ -0,0 +1,21 @@
+package com.sc.dao;
+
+import com.sc.pojo.Buyer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BuyerDaoTest {
+    @Autowired
+    BuyerDao buyerDao;
+    @Test
+    void insert(){
+        System.out.println(buyerDao.insert(new Buyer(1, 1)));
+    }
+
+    @Test
+    void findUserByBuyer(){
+        System.out.println(buyerDao.findUserByBuyer(new Buyer(1, 1)));
+    }
+}
