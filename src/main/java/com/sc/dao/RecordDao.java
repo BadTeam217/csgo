1c86cc207a19b9f31430ccdc3c0b0cb7d95300e5
woshinidiedeyeye <582965041@qq.com>
2021-07-20 00:09:48 +0800

    查询交易记录

--- src/main/java/com/sc/dao/RecordDao.java
+++ src/main/java/com/sc/dao/RecordDao.java
@@ -1,9 +1,17 @@
 package com.sc.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
 import com.sc.pojo.Record;
+import com.sc.vo.RecordVo;
 
 @Mapper
 public interface RecordDao {
 	int insertRecord(Record record);
+
+	List<RecordVo> findPageObjectByPrice(Integer buyer_id, Integer seller_id, long startIndex, long pageSize);
+
+	long getRowCount(Integer buyer_id, Integer seller_id);
 }
--- src/main/java/com/sc/service/RecordService.java
+++ src/main/java/com/sc/service/RecordService.java
@@ -1,8 +1,12 @@
 package com.sc.service;
 
+import com.sc.common.vo.PageObject;
 import com.sc.pojo.Market;
 import com.sc.pojo.User;
+import com.sc.vo.RecordVo;
 
 public interface RecordService {
-	int insertRecord(Market market,User user);
+	int insertRecord(Market market, User user);
+
+	PageObject<RecordVo> findPageObjects(Integer sellerUser_id, Integer user_id, Long pageCurrent);
 }
